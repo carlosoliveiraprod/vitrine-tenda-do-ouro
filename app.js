0a7cbde17ce1cc73a33e064cbaff9cc18aee2118
carlosoliveiraprod <carlosoliveira.prod.11@gmail.com>
2026-03-19 01:14:02 -0300

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
  * Vanilla JS dynamic injector
  */
 
-const API_URL = 'http://localhost:5678/webhook/vitrine-data'; // Endpoint fictício n8n
+const API_URL = 'http://localhost:5678/webhook/vitrine-forja'; // Endpoint fictício n8n
 const FALLBACK_MESSAGE = 'As energias estão a ser realinhadas. Volte em instantes.';
 
 async function fetchStoreData() {
